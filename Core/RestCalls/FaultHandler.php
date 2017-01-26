0cb5cad168a02ec10ad6f612c34d87810975ddba
Jhon H. Caicedo <jhcaiced@lookbooks.com>
2017-01-26 15:46:22 -0500

    LB-5424: Updated PHP SDK to v3-php-sdk-2.6.0.zip

--- Core/RestCalls/FaultHandler.php
+++ Core/RestCalls/FaultHandler.php
@@ -1,25 +1,66 @@
 <?php
 
 /**
- * Handles the fault tags in the response and handles them.
+ * Handles the 3xx, 4xx and 5xx response status code in the response and handles them.
  */
 class FaultHandler
 {
-	/**
+	 /**
 	 * The Service Context
 	 * @var ServiceContext context
 	 */
     private $context;
 
+    /**
+		If any 3xx, 4xx or 5xx status code is returned, it will store here
+		*/
+    private $httpStatusCode;
+
+    /**
+		The response body from Intuit
+		*/
+		private $responseBody;
+
+    /**
+		The call is made through OAuth, if any code other than 2xx is returned, OAuth will have an error message generated as well.
+		The OAuth error message will store here.
+		*/
+		private $oAuthHelperError;
 	/**
 	 * Initializes a new instance of the FaultHandler class.
 	 * @param ServiceContext context
 	 */
-    public function __construct($context)
+    public function __construct($context, $OAuthException)
     {
         $this->context = $context;
+				$this->generateErrorFromOAuthMsg($OAuthException);
     }
-    
+
+    /**
+		* generate Error from OAuth Exception
+		*/
+    private function generateErrorFromOAuthMsg($OAuthException)
+		{
+				if(get_class($OAuthException) == 'OAuthException'){
+					  $this->httpStatusCode = $OAuthException->getCode();
+						$this->oAuthHelperError = $OAuthException->getMessage();
+						$this->responseBody = $OAuthException->lastResponse;
+				}else{
+					throw new Exception("OAuthException required for generate error from Intuit. The passed parameters for Fault handler is not OAuthException");
+				}
+		}
+
+		public function getHttpStatusCode(){
+			return $this->httpStatusCode;
+		}
+
+		public function getOAuthHelperError(){
+			return $this->oAuthHelperError;
+		}
+
+		public function getResponseBody(){
+			return $this->responseBody;
+		}
 	/**
 	 * Parses the Response and throws appropriate exceptions.
 	 * @param WebException webException
@@ -27,6 +68,8 @@ class FaultHandler
 	 * @param bool isIpp
 	 * @return IdsException
 	 */
+	 //It was never used. Remove and run in regression test.
+	 /*
     public function ParseResponseAndThrowException($webException, $StatusCode, $isIpp = FALSE)
     {
         $idsException = NULL;
@@ -62,5 +105,6 @@ class FaultHandler
         // Return the Ids Exception.
         return $idsException;
     }
+		*/
 }
 ?>
